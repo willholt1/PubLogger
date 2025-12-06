2b76cfb7bf6b498c9ef3522d7c6b0ae6005c737b
willholt1 <will.holt.xyz@gmail.com>
2025-12-06 21:56:24 +0000

    plot locations from JSON on map

--- frontend/src/MapView.jsx
+++ frontend/src/MapView.jsx
@@ -5,7 +5,7 @@ import { api } from "./api";
 
 
 export default function MapView() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     api.get("/visits")
@@ -16,14 +16,25 @@ export default function MapView() {
       .catch((err) => console.error("API error:", err));
   }, []);
 
-    return (
-        <MapContainer
-          center={[53.40128, -2.98066]}  // Liverpool
-          zoom={13}
-          style={{ height: "100vh", width: "100%" }}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        </MapContainer>
-    );
+  return (
+    <MapContainer
+      center={[53.40128, -2.98066]} // Liverpool
+      zoom={10}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
+      {data.map((loc, i) => (
+        <Marker
+          key={i}
+          position={[loc.lat, loc.long]}  // << use your JSON coords
+        >
+          <Popup>
+            <strong>{loc.location_name}</strong><br />
+            {loc.location_type_name}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
